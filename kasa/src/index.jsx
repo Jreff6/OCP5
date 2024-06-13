9ebf3cb77a800988e7bda4001af68612d9781860
Jreff6 <enzo.solaar@hotmail.fr>
2024-06-13 21:13:50 +0200

    fixed logo

--- kasa/src/index.jsx
+++ kasa/src/index.jsx
@@ -5,17 +5,18 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
+import App from "./App";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    < App />
   </React.StrictMode>
 );
 
