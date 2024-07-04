3c8c201a338aee58b3aa50786bcfba0d14b60bc9
Jreff6 <enzo.solaar@hotmail.fr>
2024-07-04 20:14:00 +0200

    added the home return on the error page

--- kasa/src/components/error/Error.jsx
+++ kasa/src/components/error/Error.jsx
@@ -1,16 +1,28 @@
 import './error.scss'
 import React from 'react';
+import { useNavigate } from 'react-router-dom'
 
 
 
 function Error() {
   
+  const navigate = useNavigate();
+
+
+    const handleNavigateHome = () => {
+        navigate('/')
+    };
+
+
+
+
+
   return (
     <div id="main">
       <div className="wrapper">
         <p className="errorNum">404</p>
         <p className="info">Oups! La page que vous demandez n'existe pas.</p>
-        <p className='return'>Retourner sur la page d'accueil </p>
+        <p className='return' onClick={handleNavigateHome} >Retourner sur la page d'accueil </p>
       </div>      
       
     </div>
